065bf1ece9553d8dbb664beef7052008299fdf80
ayushudaydubey <102001ayushdubey@gmail.com>
2025-06-29 02:35:50 +0530

    backend url

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,18 +1,24 @@
 import axios from 'axios';
 
+const baseURL =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:3000'
+    : 'https://twofa-backend-tpgi.onrender.com';
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000', // Adjust to your backend port if different
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
