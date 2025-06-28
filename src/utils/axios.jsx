b4eb369bb56414c888ad6dce0c312c431bd2d7f1
ayushudaydubey <102001ayushdubey@gmail.com>
2025-06-29 02:45:00 +0530

    axios

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 const baseURL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:3000'
@@ -12,6 +13,7 @@ export const axiosInstance = axios.create({
   },
 });
 
+
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
@@ -21,4 +23,17 @@ axiosInstance.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+ 
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
 );
